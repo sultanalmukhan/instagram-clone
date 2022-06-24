570869c32ce219a74ca1fd03ccd9ae66231177bd
Sultan Almukhan <almukhansultan@gmail.com>
2022-06-24 16:42:22 +0600

    create project

---
+++ instagram/instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instagram
+//
+//  Created by Султан Альмухан on 24.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
